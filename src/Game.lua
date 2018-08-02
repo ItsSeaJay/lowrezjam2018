06661b93e955b3d74cf8f0f0f0a473b04b34f69c
Unknown <b6010572@my.shu.ac.uk>
2018-08-02 17:10:20 +0000

    Added game class

---
+++ src/Game.lua
@@ -0,0 +1,16 @@
+local class = require "lib.classic"
+local Game = class:extend()
+
+function Game:new()
+
+end
+
+function Game:update()
+	
+end
+
+function Game:draw()
+	
+end
+
+return Game
--- src/GameObject.lua
+++ src/GameObject.lua
@@ -2,7 +2,15 @@ local class = require "lib.classic"
 local GameObject = class:extend()
 
 function GameObject:new()
+	self.destroyed = false
+end
 
+function GameObject:draw()
+	love.graphics.draw(
+		self.image,
+		self.x,
+		self.y
+	)
 end
 
 return GameObject

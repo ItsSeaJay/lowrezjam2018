a6438958d8aebb01f77305b9485e6a77f7d030ce
Unknown <b6010572@my.shu.ac.uk>
2018-08-02 09:12:17 +0000

    Added player object

--- main.lua
+++ main.lua
@@ -1,13 +1,15 @@
-function love.load()
+local Player = require "src.Player"
 
+function love.load()
+	player = Player()
 end
 
 function love.update(deltaTime)
-
+	player:update()
 end
 
 function love.draw(deltaTime)
-
+	player:draw()
 end
 
 function love.keypressed(key, scancode, isRepeat)
---
+++ src/Player.lua
@@ -0,0 +1,16 @@
+local class = require "lib.classic"
+local Player = class:extend()
+
+function Player:new()
+
+end
+
+function Player:update(deltaTime)
+
+end
+
+function Player:draw()
+	-- love.graphics.draw()
+end
+
+return Player

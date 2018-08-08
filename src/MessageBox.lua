7d9f85b6bb7d4cbb4a17addf3b2f12f81a802d1a
Unknown <b6010572@my.shu.ac.uk>
2018-08-08 10:01:26 +0000

    Message boxes now draw a text object

--- src/MessageBox.lua
+++ src/MessageBox.lua
@@ -1,18 +1,25 @@
+local lume = require "lib.lume"
 local class = require "lib.classic"
 local erogodic = require "lib.erogodic"() -- This one returns a function
 
 local fonts = require "src.fonts"
-local message = require "src.messages.test"
+local script = require "src.messages.test"
 
 local MessageBox = class:extend()
 
 function MessageBox:new()
-	self.script = erogodic(message)
+	self.script = erogodic(script)
 	self.message = {}
-	self.message.target = self.script:next().msg
+	self.message.target = lume.wordwrap(self.script:next().msg, 16)
 	self.message.current = ""
 	self.message.delta = 0
-	self.message.speed = 8 -- Characters per second
+	self.message.speed = 16
+	self.text = love.graphics.newText(
+		fonts.tomThumbNew,
+		self.message.current
+	)
+	self.characterLimit = 18
+	self.margin = 2
 	self.visible = true
 end
 
@@ -22,25 +29,38 @@ function MessageBox:update(deltaTime)
 		self.message.delta + self.message.speed * deltaTime,
 		string.len(self.message.target)
 	)
-	self.message.current = string.sub(self.message.target, 0, self.message.delta)
+	-- Set the current message to a substring of the target based on
+	-- how far we are through the message
+	self.message.current = string.sub(
+		self.message.target,
+		0,
+		self.message.delta
+	)
+	-- Configure the text to use the current message at all times
+	self.text:set(self.message.current)
 end
 
 function MessageBox:draw()
-	love.graphics.setFont(fonts.tomThumbNew)
-	
 	if self.visible then
-		love.graphics.print(self.message.current)
+		love.graphics.draw(
+			self.text
+		)
 	end
 end
 
 function MessageBox:advance()
     if self.script:hasNext() then
         if self.message.delta < string.len(self.message.target) then
+        	-- Skip the scrolling and show the whole message
             self.message.delta = string.len(self.message.target)
         else
             local node = self.script:next()
+
             if node then
-                self.message.target = node.msg
+                self.message.target = lume.wordwrap(
+                	node.msg,
+                	self.characterLimit
+                )
                 self.message.current = ""
                 self.message.delta = 0
             else
--- src/messages/test.lua
+++ src/messages/test.lua
@@ -1,4 +1,7 @@
 return function ()
-	msg "Hello, World!"
-	msg "MSG is a chemical,\nI think."
+	msg "The quick brown fox jumps over the lazy dog."
+	msg "Sphinx of black quartz, judge my vow!"
+	msg "Pack my box with five dozen liqour jugs."
+	msg "The quick onyx goblin jumps over the lazy dwarf."
+	msg "Cwm fjord bank glyphs vext quiz."
 end

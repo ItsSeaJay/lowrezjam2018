e2deb94d4945b810316f020b7d4bc3470c8126fd
Unknown <b6010572@my.shu.ac.uk>
2018-08-18 09:26:14 +0000

    Fixed zip subfolder

--- build.bat
+++ build.bat
@@ -1,17 +1,16 @@
 @echo off
 
-rem NOTE: This script assumes that 7-Zip is installed on the Windows system PATH
+rem NOTE: This script assumes that both 7-Zip and LOVE
+rem       are installed on the Windows system PATH
 
 set name=lowrezjam2018
+set lovedir=D:\LOVE
 
 rem Make a build folder if one doesn't already exist
 mkdir build
 
 rem Clear the contents of the build folder
-del build\%name%.zip
-del build\%name%_windows.zip
-del build\%name%.love
-del build\%name%.exe
+del build\*
 
 rem Compress the contents of the repo into a zipped folder
 7z a -r build\%name%.zip
@@ -19,6 +18,6 @@ rem Create a copy of that zipped folder with the extension changed around
 copy build\%name%.zip build\%name%.love
 rem Append the contents of the love2d executable to the end of the archive
 rem to create an executable file
-copy /b D:\LOVE\love.exe+build\%name%.love build\%name%.exe
+copy /b %lovedir%\love.exe+build\%name%.love build\%name%.exe
 rem Add that executable along with any necessary resources to a final zip folder
-7z a build\%name%_windows.zip -i!build\%name%.exe -i!bin\love.dll
+7z a build\%name%_windows.zip .\build\%name%.exe .\bin\* %lovedir%\license.txt
